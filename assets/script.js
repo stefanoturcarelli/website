0f5124e1239af984e28be4dea6e0d8fea2205ccc
Stefano <stefano.turcarelli@icloud.com>
2023-11-19 11:38:28 -0600

    Add arrow and circle that follows the cursor

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,10 @@
 "use strict";
 
-// Add event listener
-function onEvent(event, selector, callback) {
-  return selector.addEventListener(event, callback);
-}
+// const circle = document.querySelector("#circle");
 
-// Get HTML element by id
-function getElement(selector, parent = document) {
-  return parent.getElementById(selector);
-}
-
-// Select HTML element
-function select(selector, parent = document) {
-  return parent.querySelector(selector);
-}
-
-// Get a (node) list of HTML elements as array
-function selectAll(selector, parent = document) {
-  return [...parent.querySelectorAll(selector)];
-}
-
-// Print
-function print(arg) {
-  console.log(arg);
-}
-
-// Sleep
-function sleep(duration) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration);
-  });
-}
-
-// Generate random number between - and including - 'min' and 'max'
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-// Filter array
-function filterArray(array, callback) {
-  return array.filter(callback);
-}
-
-// Create an HTML element
-function create(element, parent = document) {
-  return parent.createElement(element);
-}
+// document.addEventListener("mousemove", (event) => {
+//   const x = event.clientX - circle.clientWidth / 2;
+//   const y = event.clientY - circle.clientHeight / 2;
+//   circle.style.left = x + "px";
+//   circle.style.top = y + "px";
+// });
