ac8d4092fbe023d0d7f19970748e1235888919ad
Stefano <stefano.turcarelli@icloud.com>
2024-03-11 16:17:35 -0500

    Refactor code and change hero banner background

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
-// const circle = document.querySelector("#circle");
+const circle = document.querySelector("#circle");
 
-// document.addEventListener("mousemove", (event) => {
-//   const x = event.clientX - circle.clientWidth / 2;
-//   const y = event.clientY - circle.clientHeight / 2;
-//   circle.style.left = x + "px";
-//   circle.style.top = y + "px";
-// });
+document.addEventListener("mousemove", (event) => {
+  const x = event.clientX - circle.clientWidth / 2;
+  const y = event.clientY - circle.clientHeight / 2;
+  circle.style.left = x + "px";
+  circle.style.top = y + "px";
+});
