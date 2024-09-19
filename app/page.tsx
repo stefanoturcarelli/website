101790bf2dfb16db83d78c788f00813fa2a38917
Stefano <stefano.turcarelli@outlook.com>
2024-09-19 13:41:15 -0500

    Add ProjectCard component to IndexPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ProjectCard } from "./components/projectCard/ProjectCard";
 
 export default function IndexPage() {
   return (
@@ -15,75 +16,57 @@ export default function IndexPage() {
         </p>
         <br />
         <h2>Projects</h2>
-        <h3>Catering Business Website</h3>
-        <p>
-          A website for a catering business designed to promote and provide
+        <ProjectCard
+          title="Catering Business Website"
+          description=" A website for a catering business designed to promote and provide
           information about a food catering services business based in Buenos
           Aires, Argentina. The website is built with HTML, CSS, and JavaScript.
           The website is hosted on GitHub Pages.
-        </p>
-        <p>
-          (In Progress) Allow customers to place orders online. Allow the
+          (Under Development) Allow customers to place orders online. Allow the
           business owner to manage customer, orders, update the menu, and
           display the analytics of the business. The website is being developed
           with Next.js, TypeScript, React, Redux, and Vercel.
-        </p>
+          "
+          link1="https://cateringbiencasero.com/"
+          link2="https://github.com/stefanoturcarelli/catering-bien-casero"
+          link1Text="Visit Website"
+          link2Text="Repository"
+        />
         <br />
-        <h3>Fast Typing Game</h3>
-        <p>
-          A typing game that challenges players to type as many words as they
+        <ProjectCard
+          title="Fast Typing Game"
+          description=" A typing game that challenges players to type as many words as they
           can in 20 seconds. The game is built with JavaScript. The game is
-          hosted on GitHub Pages.
-        </p>
-        <a
-          href="https://stefanoturcarelli.github.io/fast-typing-game/"
-          target="_blank"
-        >
-          Play Game
-        </a>
-        <a
-          href="https://github.com/stefanoturcarelli/fast-typing-game"
-          target="_blank"
-        >
-          Repository
-        </a>
+          hosted on GitHub Pages."
+          link1="https://stefanoturcarelli.github.io/fast-typing-game/"
+          link2="https://github.com/stefanoturcarelli/fast-typing-game"
+          link1Text="Play Game"
+          link2Text="Repository"
+        />
         <br />
-        <h3>JobLink</h3>
-        <p>
-          A job search website that uses web browser storage to login users and
+        <ProjectCard
+          title="JobLink"
+          description="A mock job search website that uses web browser storage to login users and
           allow them to post text and images. The website is built with HTML,
-          CSS, and JavaScript. The website is hosted on GitHub Pages.
-        </p>
-        <p>
-          (In Progress) Allow users to to search for jobs by location and job
+          CSS, and JavaScript. The website is hosted on GitHub Pages. (Under Development) Allow users to to search for jobs by location and job
           title, create an account, save jobs, and apply to jobs. The website is
-          being developed with Next.js, TypeScript, React, Redux, and Vercel.
-        </p>
-        <a href="https://joblink.vercel.app/" target="_blank">
-          Visit Website
-        </a>
-        <a href="wellfc.github.io/joblink/" target="_blank">
-          Repository
-        </a>
-        <br />
-        <h3>Essential Oils Website</h3>
-        <p>
-          A mock website for an essential oils business designed to promote and
+          being developed with Next.js, TypeScript, React, Redux, and Vercel."
+          link1="https://wellfc.github.io/joblink/"
+          link2="wellfc.github.io/joblink/"
+          link1Text="Visit Website"
+          link2Text="Repository"
+        />
+        <ProjectCard
+          title="Essential Oils Website"
+          description="A mock website for an essential oils business designed to promote and
           provide information about essential oils. The website is built
-          WordPress, Xamp, and Divi The website is hosted on GitHub Pages.
-        </p>
-        <a
-          href="https://stefanoturcarelli.github.io/essential-oils-website/"
-          target="_blank"
-        >
-          Visit Website
-        </a>
-        <a
-          href="https://github.com/stefanoturcarelli/essential-oils-website"
-          target="_blank"
-        >
-          Repository
-        </a>
+          WordPress, Xamp, and Divi The website is hosted on GitHub Pages."
+          link1="https://stefanoturcarelli.github.io/essential-oils-website/"
+          link2="https://github.com/stefanoturcarelli/essential-oils-website"
+          link1Text="Visit Website"
+          link2Text="Repository"
+        />
+        <br />
       </div>
     </>
   );
